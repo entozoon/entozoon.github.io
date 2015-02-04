ab72f215332717efa0cb9eb18d10e80c67f33701
myke <michael@evoluted.net>
2015-02-04 13:59:04 +0000

    initial

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Test</title>
+	<meta name="description" content="Lorem ipsum" />
+
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+	<meta name="viewport" content="initial-scale=1.0, minimum-scale=1.0" />
+</head>
+<body>
+<div class="wrapper">
+	Hello, the date is <?php echo Date('Y-m-d'); ?>
+</div>
+</body>
+</html>
